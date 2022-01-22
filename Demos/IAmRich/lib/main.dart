2c74bb7143597e2632fed529bb1d37cedf2fc1db
Brennan Rafters <brennanrafters@Brennans-Mac-mini.local>
2022-01-22 19:46:10 -0400

    Created my first Flutter app

--- Demos/IAmRich/lib/main.dart
+++ Demos/IAmRich/lib/main.dart
@@ -2,12 +2,22 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
+    MaterialApp(
       // Most projects start off with 'MaterialApp' as the first widget
       // home: is where all of the content starts
-      home: Center(
-        // Centers Widgets
-        child: Text("Hello World"),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('I Am Rich'),
+          foregroundColor: const Color.fromRGBO(242, 233, 220, 1),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        backgroundColor: const Color.fromRGBO(242, 233, 220, 1),
+        body: const Center(
+          child: Image(
+            image: NetworkImage(
+                'https://images.unsplash.com/photo-1534353436294-0dbd4bdac845?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80'),
+          ),
+        ),
       ),
     ),
   );
