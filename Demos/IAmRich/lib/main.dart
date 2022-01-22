8ea9e7365fbf674d5ddcfb3953a74b4e3caf8890
Brennan Rafters <brennanrafters@Brennans-Mac-mini.local>
2022-01-22 18:47:05 -0400

    Created an initial program that centers text and displays 'Hello World'

---
+++ Demos/IAmRich/lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      // Most projects start off with 'MaterialApp' as the first widget
+      // home: is where all of the content starts
+      home: Center(
+        // Centers Widgets
+        child: Text("Hello World"),
+      ),
+    ),
+  );
+}
